0431eadc131f9db9e0a6a2d2d25170645a46b1c2
Levente Csernus <kiscsernus98@gmail.com>
2019-12-14 17:55:04 +0100

    made the listbox to work

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp1.Entities;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +21,20 @@ namespace WindowsFormsApp1
             label2.Text = NamePairs.FirstName;
             button1.Text = NamePairs.Add;
 
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                FirstName = label2.Text,
+                LastName = label1.Text
+            };
+            users.Add(u);
         }
     }
 }
